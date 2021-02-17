1c8bb7b17d139f8d894fbbaa097d24ffc10cb6e8
Killkase42 <nicholasvourlas@gmail.com>
2021-02-16 23:13:09 -0500

    Java Files

---
+++ src/com/company/Client.java
@@ -0,0 +1,17 @@
+/*
+ *Nicholas Vourlas
+ *February 19, 2020
+ * ICS4U-01
+ * Mr. Hofstatter
+ * Local Bank Assignment
+ * [Code description]
+ */
+
+package com.company;
+
+public class Client {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
